4a7f7427282178efedee66dc00f8e5c9c87af956
Alan Sebastian <alan8seb@gmail.com>
2026-08-15 18:10:24 +0930

    Multipl by 2 and divide by 6 Solution, key is number of prime factors for 2 and 3

--- Multiply_by_2_Divide_by_6.cpp
+++ Multiply_by_2_Divide_by_6.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-
-int main(){
+int main() {
     int t;
     cin >> t;
-    vector<int> storage()
     vector<int> ans(t);
-    for (int i = 0; i < t; i++){
-        int n;
-        cin >> n;
-        if (n % 6 != 6 || n % 6 != 3){
+    int num = 0;
+
+    for (int i = 0; i < t; i++) {
+        cin >> num;
+        int count2 = 0;
+        int count3 = 0;
+        // Count prime factors of 2
+        while (num % 2 == 0) {
+            num /= 2;
+            count2++;
+        }
+        // Count prime factors of 3
+        while (num % 3 == 0) {
+            num /= 3;
+            count3++;
+        }
+
+        if (num != 1) {
             ans[i] = -1;
         }
-        else{
-            while (n != 1){
-
-            }
+        // You cannot have more 2s than 3s
+        else if (count3 < count2) {
+            ans[i] = -1;
         }
-        return 0;
+        else {
+            ans[i] = 2 * count3 - count2;
+        }
     }
+    for (int answer : ans) {
+        cout << answer << endl;
+    }
+
     return 0;
 }
+
